d9f44101158c8b524516e78194cf882f585db176
menya <test@example.com>
2016-09-20 22:05:04 +0500

    forgot about gzip

--- server/index.js
+++ server/index.js
@@ -6,6 +6,22 @@ const http = require('http');
 const dist = __dirname + '/../dist';
 const PORT = process.env.PORT;
 
+app.use(function (req, res, next) {
+
+	if ('production' === process.env.NODE_ENV) {
+
+		if ('/app.js' === req.url) {
+			res.setHeader('Content-Encoding', 'gzip');
+			res.removeHeader('Content-Length');
+			req.url = '/app.js.gz';
+		}
+
+	}
+
+	next();
+
+});
+
 app.use('/', express.static(dist));
 
 app.all('/*', function all(req, res) {
